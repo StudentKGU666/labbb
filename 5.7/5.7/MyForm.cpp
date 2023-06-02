247f9ebd9b15f9d194c191b1543162b8ab995e95
StudentKGU666 <114594034+StudentKGU666@users.noreply.github.com>
2023-06-03 00:38:38 +0300

    Add files via upload

---
+++ 5.7/5.7/MyForm.cpp
@@ -0,0 +1,114 @@
+﻿#include "MyForm.h"
+#include "vector"
+using namespace My57;
+
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm);
+
+	return 0;
+}
+
+//конвертируем std::string в System::string^
+String^ Convert_string_to_String(std::string& os) {
+	System::String^ str = gcnew System::String(os.c_str());
+
+	return str;
+}
+
+struct JewelryInfo
+{
+	string name;
+	double price;
+	double gemstone_weight;
+	string gemstone_name;
+};
+
+System::Void My57::MyForm::button1_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	vector<JewelryInfo> jewelry_list;
+	double total_gemstone_weight = 0;
+	double total_price = 0;
+	int sapphire_count = 0;
+	int diamond_count = 0;
+	int emerald_count = 0;
+	int ruby_count = 0;
+	string str;
+
+	jewelry_list.push_back({ "Кольцо с бриллиантом", 50000, 0.3, "Бриллиант" });
+	jewelry_list.push_back({ "Ожерелье с сапфирами", 75000, 1.0, "Сапфир" });
+	jewelry_list.push_back({ "Серьги с изумрудами", 60000, 0.5, "Изумруд" });
+	jewelry_list.push_back({ "Браслет с рубинами", 90000, 0.7, "Рубин" });
+	jewelry_list.push_back({ "Кольцо с изумрудом", 80000, 0.4, "Изумруд" });
+	jewelry_list.push_back({ "Серьги с бриллиантами", 120000, 0.8, "Бриллиант" });
+
+	for (int i = 0; i < jewelry_list.size(); i++) {
+		total_price += jewelry_list[i].price;
+		if (jewelry_list[i].gemstone_name != "")
+		{
+			total_gemstone_weight += jewelry_list[i].gemstone_weight;
+		}
+		if (jewelry_list[i].gemstone_name == "Сапфир")
+		{
+			sapphire_count++;
+		}
+		if (jewelry_list[i].gemstone_name == "Бриллиант")
+		{
+			diamond_count++;
+		}
+		if (jewelry_list[i].gemstone_name == "Изумруд")
+		{
+			emerald_count++;
+		}
+		if (jewelry_list[i].gemstone_name == "Рубин")
+		{
+			ruby_count++;
+		}
+		textBox4->Text += Convert_string_to_String(jewelry_list[i].name) + "\r\n" + " Стоимость: " + jewelry_list[i].price + "\r\n" + " Вес: " + jewelry_list[i].gemstone_weight + "\r\n";
+	}
+	textBox1->Text += total_gemstone_weight;
+	textBox2->Text += total_price;
+	textBox3->Text += sapphire_count;
+	button1->Enabled = false;
+}
+
+System::Void My57::MyForm::textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e)
+{
+	return System::Void();
+}
+
+System::Void My57::MyForm::textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e)
+{
+	return System::Void();
+}
+
+System::Void My57::MyForm::textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e)
+{
+	return System::Void();
+}
+
+System::Void My57::MyForm::textBox4_TextChanged(System::Object^ sender, System::EventArgs^ e)
+{
+	return System::Void();
+}
+
+System::Void My57::MyForm::label1_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	return System::Void();
+}
+
+System::Void My57::MyForm::label2_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	return System::Void();
+}
+
+System::Void My57::MyForm::label3_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	return System::Void();
+}
+
+
+
---
+++ 5.7/5.7/MyForm.h
@@ -0,0 +1,214 @@
+﻿#pragma once
+#include <Windows.h>
+#include <string>
+using namespace std;
+
+namespace My57 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^ textBox1;
+	protected:
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::Label^ label4;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(13, 27);
+			this->textBox1->Margin = System::Windows::Forms::Padding(2);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->ReadOnly = true;
+			this->textBox1->Size = System::Drawing::Size(104, 20);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(15, 67);
+			this->textBox2->Margin = System::Windows::Forms::Padding(2);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->ReadOnly = true;
+			this->textBox2->Size = System::Drawing::Size(104, 20);
+			this->textBox2->TabIndex = 1;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox2_TextChanged);
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(15, 107);
+			this->textBox3->Margin = System::Windows::Forms::Padding(2);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->ReadOnly = true;
+			this->textBox3->Size = System::Drawing::Size(107, 20);
+			this->textBox3->TabIndex = 2;
+			this->textBox3->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox3_TextChanged);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(10, 9);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(234, 16);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Вес всех драгоценных камней:";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(11, 49);
+			this->label2->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(225, 16);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Общая стоимость украшений:";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(11, 89);
+			this->label3->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(285, 16);
+			this->label3->TabIndex = 5;
+			this->label3->Text = L"Количество украшений с сапфирами:";
+			this->label3->Click += gcnew System::EventHandler(this, &MyForm::label3_Click);
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(296, 477);
+			this->button1->Margin = System::Windows::Forms::Padding(2);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(114, 24);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"Посчитать";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(13, 195);
+			this->textBox4->Multiline = true;
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->ReadOnly = true;
+			this->textBox4->Size = System::Drawing::Size(240, 306);
+			this->textBox4->TabIndex = 8;
+			this->textBox4->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox4_TextChanged_1);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(12, 179);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(196, 16);
+			this->label4->TabIndex = 9;
+			this->label4->Text = L"Общая сводка украшений";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(435, 536);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Margin = System::Windows::Forms::Padding(2);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	public: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void textBox4_TextChanged(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void label3_Click(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void textBox4_TextChanged_1(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
+}
