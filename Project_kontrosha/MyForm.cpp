eed304da490006dff9a091cb9acf12245a17b9cf
StudentKGU666 <114594034+StudentKGU666@users.noreply.github.com>
2023-06-04 12:15:58 +0300

    Update MyForm.cpp

--- Project_kontrosha/MyForm.cpp
+++ Project_kontrosha/MyForm.cpp
@@ -12,7 +12,7 @@ using namespace System::Windows::Forms;
 using namespace msclr::interop;
 
 
-//конвертируем std::string в System::string^
+// std::string РІ System::string^
 String^ Convert_string_to_String(std::string& os)
 {
 	System::String^ str = gcnew System::String(os.c_str());
@@ -22,8 +22,8 @@ String^ Convert_string_to_String(std::string& os)
 
 bool isAsciiSymbol(char c) 
 {
-	int ascii_code = static_cast<int>(c); // получаем код ASCII символа
-	return (ascii_code >= 32 && ascii_code <= 126); // проверяем, находится ли он в диапазоне
+	int ascii_code = static_cast<int>(c); 
+	return (ascii_code >= 32 && ascii_code <= 126); 
 }
 
 [System::STAThreadAttribute()]
@@ -57,29 +57,29 @@ System::Void Projectkontrosha::MyForm::button1_Click(System::Object^ sender, Sys
 	}
 	
 	textBox3 -> Text = text;
-	int totalLetters = 0; // общее число алфавитных символов
+	int totalLetters = 0; 
 	string letter;
-	map<char, int> letterCounts; // словарь для хранения количества вхождений каждого символа
+	map<char, int> letterCounts; 
 	
 	if (count_once == 0)
 	{
 		for (char c : str)
 		{
-			if (isalpha(c)) // проверка, является ли символ буквой латинской
+			if (isalpha(c)) 
 			{				
 				c = tolower(c);
-				letterCounts[c]++; // увеличение счетчика вхождений данного символа
+				letterCounts[c]++; 
 				totalLetters++;
 			}
 		}
 		if (totalLetters != 0)
 		{
 			for (auto p : letterCounts)
-			{ // перебор всех символов и вывод гистограммы
+			{ 
 				char c = p.first;
-				int count = p.second; //Манипуляции шобы букву нормально вывести, а не ascii код
+				int count = p.second; 
 				letter = (char)c;
-				double frequency = (double)count / totalLetters * 100; // вычисление частоты вхождения символа
+				double frequency = (double)count / totalLetters * 100; 
 				textBox2->Text += Convert_string_to_String(letter) + ": " + Convert_string_to_String(string(count, '*')) + " (" + frequency + "%)" + "\r\n";
 				textBox1->Text = "";
 			}
@@ -87,14 +87,14 @@ System::Void Projectkontrosha::MyForm::button1_Click(System::Object^ sender, Sys
 		else
 		{
 			textBox3->Text = textBox1->Text;
-			textBox2->Text = "В строке не найдены буквы английского алфавита";
+			textBox2->Text = "Р’ С‚РµРєСЃС‚Рµ РЅРµ РЅР°Р№РґРµРЅС‹ Р°РЅРіР»РёР№СЃРєРёРµ СЃРёРјРІРѕР»С‹";
 			textBox1->Text = "";
 		}
 	}
 	else 
 	{
 		textBox3->Text = textBox1->Text;
-		textBox2->Text = "Кириллица в программе недопустима!" + count_once;
+		textBox2->Text = "РљРёСЂРёР»Р»РёС†Р° РЅРµРґРѕРїРїСѓСЃС‚РёРјР°!" + count_once;
 		textBox1->Text = "";
 	}
 }
